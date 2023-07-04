49190785c02c8efc0344bb855df3072998d29ed0
elPablo <paha.kashkarov@gmail.com>
2023-07-04 16:30:03 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd(){
+        int numbersCount = 1;
+        PhoneBook phoneBook = new PhoneBook();
+
+        int counter = phoneBook.add();
+
+        assertEquals(numbersCount, counter);
+
+    }
 }
